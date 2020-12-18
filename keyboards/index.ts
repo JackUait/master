5cf5f3ebc03aebbf84b4c164abd8703bb5544118
Evgeniy Pyatkov <jackuait@bk.ru>
2020-12-18 03:56:29 -0800

    updated template

--- keyboards/index.ts
+++ keyboards/index.ts
@@ -1,7 +1,18 @@
-export const yourNameGoesHere = {
+// обычная клавиатура
+export const keyboard = {
     keyboard: [
         [{
             text: 'Название кнопки'
         }]
+    ]
+}
+
+// инлайн клавиатура
+export const inlineKeyboard = {
+    inline_keyboard: [
+        [
+            {text: 'Название кнопки', callback_data: 'button1'},
+            {text: 'Ссылка', url: 'https://google.com'}
+        ]
     ]
 }
